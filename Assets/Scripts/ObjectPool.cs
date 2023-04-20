626f6df3b262fa532f358162876a0828db89b055
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-04-20 11:35:47 +0900

    Add LogError / Inheritance of SingletonMonoBehaviour

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -2,106 +2,120 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+namespace GameTool.ObjectPool
 {
-    [SerializeField] 
-    List<ObjectPoolParam> _poolObj = default;
-
-    [SerializeField] 
-    Transform _poolParent;
-
-    List<ObjectParam> _pools = new List<ObjectParam>();
-
-    void Awake()
+    public class ObjectPool : SingletonMonoBehaviour<ObjectPool>
     {
-        InstantiatePool();
-    }
+        [SerializeField]
+        List<ObjectPoolParam> _poolObj = default;
 
-    private void InstantiatePool()
-    {
-        foreach(var obj in _poolObj)
+        [SerializeField]
+        Transform _poolParent;
+
+        List<ObjectParam> _pools = new List<ObjectParam>();
+
+        protected override void Awake()
         {
-            for(int i = 0; i < obj.MaxCount; i++)
-            {
-                var prefab = Instantiate(obj.GameObject, _poolParent);
-                prefab.name = obj.Name;
-                prefab.SetActive(false);
-                _pools.Add(new ObjectParam(prefab.name, prefab));
-            }
+            base.Awake();
+            InstantiatePool();
         }
-    }
 
-    public GameObject UseObject(string name, Vector3 pos)
-    {
-        if (name is null) return null;
-
-        foreach(var pool in _pools)
+        private void InstantiatePool()
         {
-            if(!pool.GameObject.activeSelf && pool.Name == name)
+            foreach (var obj in _poolObj)
             {
-                pool.GameObject.transform.position = pos;
-                pool.GameObject.SetActive(true);
-                return pool.GameObject;
+                for (int i = 0; i < obj.MaxCount; i++)
+                {
+                    var prefab = Instantiate(obj.GameObject, _poolParent);
+                    prefab.name = obj.Name;
+                    prefab.SetActive(false);
+                    _pools.Add(new ObjectParam(prefab.name, prefab));
+                }
             }
         }
 
-        var searchObj = _pools.Find(x => x.Name == name);
-
-        if (searchObj is null) return null;
-
-        var prefab = Instantiate(searchObj.GameObject, _poolParent);
-        prefab.name = searchObj.Name;
-        prefab.transform.position = pos;
-        prefab.SetActive(true);
-        _pools.Add(new ObjectParam(prefab.name, prefab));
-
-        return prefab;
-    }
-
-    public T UseObject<T>(string name, Vector3 pos) where T : Component
-    {
-        if(UseObject(name, pos).TryGetComponent(out T component))
+        public GameObject UseObject(string name, Vector3 pos)
         {
-            return component;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Debug.LogError($"名前を指定してください");
+                return null;
+            }
+
+            foreach (var pool in _pools)
+            {
+                if (!pool.GameObject.activeSelf && pool.Name == name)
+                {
+                    pool.GameObject.transform.position = pos;
+                    pool.GameObject.SetActive(true);
+                    return pool.GameObject;
+                }
+            }
+
+            var searchObj = _pools.Find(x => x.Name == name);
+
+            if (searchObj is null)
+            {
+                Debug.LogError($"オブジェクト:{name}が存在しません");
+                return null;
+            }
+
+            var prefab = Instantiate(searchObj.GameObject, _poolParent);
+            prefab.name = searchObj.Name;
+            prefab.transform.position = pos;
+            prefab.SetActive(true);
+            _pools.Add(new ObjectParam(prefab.name, prefab));
+
+            return prefab;
         }
-        else
+
+        public T UseObject<T>(string name, Vector3 pos) where T : Component
         {
-            return null;
+            if (UseObject(name, pos).TryGetComponent(out T component))
+            {
+                return component;
+            }
+            else
+            {
+                Debug.LogError($"オブジェクト:{name}にコンポーネント:{component}が存在しません");
+                return null;
+            }
+        }
+
+    }
+
+    [Serializable]
+    public class ObjectParam
+    {
+        public string Name { get; private set; }
+
+        public GameObject GameObject { get; private set; }
+
+        public ObjectParam(string name, GameObject gameObject)
+        {
+            Name = name;
+            GameObject = gameObject;
         }
     }
 
-}
-
-[Serializable]
-public class ObjectParam
-{
-    public string Name { get; private set; }
-
-    public GameObject GameObject { get; private set; }
-
-    public ObjectParam(string name, GameObject gameObject)
+    [Serializable]
+    public class ObjectPoolParam
     {
-        Name = name;
-        GameObject = gameObject;
+        [SerializeField]
+        private string name;
+
+        [SerializeField]
+        private GameObject gameObject;
+
+        [SerializeField, Min(0)]
+        private int maxCount;
+
+        public string Name => name;
+
+        public GameObject GameObject => gameObject;
+
+        public int MaxCount => maxCount;
+
     }
-}
-
-[Serializable]
-public class ObjectPoolParam
-{
-    [SerializeField] 
-    private string name;
-    
-    [SerializeField] 
-    private GameObject gameObject;
-    
-    [SerializeField,Min(0)] 
-    private int maxCount;
-
-    public string Name => name;
-
-    public GameObject GameObject => gameObject;
-
-    public int MaxCount => maxCount;
 
 }
